eb9d55fe99966e395e3f2b7f4590fa60b31589dc
Gusts Linkevics <gusts@gst.lv>
2023-08-07 21:32:26 +0300

    Add project files.

---
+++ WebMVC-0506/Data/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace WebMVC_0506.Data.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+}
---
+++ WebMVC-0506/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMVC_0506.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
