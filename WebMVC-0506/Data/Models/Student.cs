03c8cadf4a2cfebd6988d4458b6c9ae7efcd3784
Gusts Linkevics <gusts@gst.lv>
2023-08-08 19:32:04 +0300

    Migrations enabled

--- WebMVC-0506/Data/Models/Student.cs
+++ WebMVC-0506/Data/Models/Student.cs
@@ -5,6 +5,8 @@
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+
+        public DateTime? BirthDate { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
