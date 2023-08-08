ade97d0b77559bea5e64299ec0577c0f447ec00f
Gusts Linkevics <gusts@gst.lv>
2023-08-08 21:22:24 +0300

    Scafolding used

--- WebMVC-0506/Data/Models/Course.cs
+++ WebMVC-0506/Data/Models/Course.cs
@@ -9,6 +9,6 @@ namespace WebMVC_0506.Data.Models
         public string Title { get; set; }
         public int Credits { get; set; }
 
-        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
     }
 }
