6399165748275b08338f90fc4033decf0010323e
Gusts Linkevics <gusts@gst.lv>
2023-08-08 19:04:43 +0300

    Database inicialization added

--- WebMVC-0506/Program.cs
+++ WebMVC-0506/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using WebMVC_0506.Data;
 
 namespace WebMVC_0506
@@ -33,7 +34,27 @@ namespace WebMVC_0506
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            CreateDbIfNotExists(app);
+
             app.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
     }
 }
